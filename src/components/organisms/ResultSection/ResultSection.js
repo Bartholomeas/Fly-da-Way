65e89a8cfd11477fa98a45949f881078c93d8c3d
Bartosz <weivan321@gmail.com>
2021-08-03 19:09:04 +0200

    deleted files wtf

---
+++ src/components/organisms/ResultSection/ResultSection.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { ResultWrapper, FillInfo, ResultInfo } from "./ResultSection.styles";
+import FlyCard from "components/molecules/FlyCard/FlyCard";
+
+const ResultSection = ({ flyQuotes, flightInfos }) => {
+  console.log(flightInfos);
+
+  const checkCarrier = () => {};
+
+  return (
+    <ResultWrapper>
+      <ResultInfo>Press card for more info</ResultInfo>
+      {flyQuotes.length > 0 ? (
+        flyQuotes.map((card) => {
+          let carrierName;
+          // eslint-disable-next-line no-unused-vars
+          const cardCarrier = flightInfos.carriers.forEach((carrier) => {
+            if (card.OutboundLeg.CarrierIds[0] === carrier.CarrierId) {
+              carrierName = carrier.Name;
+            }
+          });
+          console.log(card);
+
+          return (
+            <FlyCard
+              key={card.QuoteId}
+              from={card.OutboundLeg.OriginId}
+              to={card.OutboundLeg.DestinationId}
+              price={card.MinPrice}
+              depDate={card.OutboundLeg.DepartureDate}
+              direct={card.Direct}
+              carrierId={carrierName}
+            />
+          );
+        })
+      ) : (
+        <FillInfo>
+          Fill required fields <br /> and search for your results.
+        </FillInfo>
+      )}
+    </ResultWrapper>
+  );
+};
+
+export default ResultSection;
+
+// card.OutboundLeg.CarrierIds[0]
